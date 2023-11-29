957e13f87100b308aa1da20fe2ec7e366addda1d
chaulh8 <chaulh8@ghtk.co>
2023-11-29 10:18:38 +0700

    Init project

---
+++ Chapter/chapter1.py
@@ -0,0 +1,17 @@
+import cv2
+
+# cap = cv2.VideoCapture("../Resource/DaDaDa.mp4")
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10,100)
+#check open
+if(cap.isOpened()==False):
+    print("Error opening video stream or file")
+
+while True:
+
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
